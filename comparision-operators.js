6b886d0ace054771a81c1f0806321f899c81ad83
mounika <mounikamusham12@gmail.com>
2021-12-14 18:09:53 +0530

    comparision-operators

---
+++ comparision-operators.js
@@ -0,0 +1,74 @@
+if(3 == "3"){
+    console.log(true);
+}else{
+    console.log(false);
+}
+
+if(1 === "1"){            
+    console.log(true);
+}else{
+    console.log(false);
+}
+
+if(3 != "3"){
+    console.log(true);
+}else{
+    console.log(false);
+}
+if("5" !== "5"){                
+    console.log(true);
+}else{
+    console.log(false);
+}
+
+if(8 < "8"){
+    console.log(true);
+}else{
+    console.log(false);
+}
+//
+
+if(2 <="2"){
+    console.log(true);
+}else{
+    console.log(false);
+}
+//
+if(3 > "2"){
+    console.log(true);
+}else{
+    console.log(false);
+}
+
+//
+
+if(5 >= "5"){
+    console.log(true);
+}else{
+    console.log(false);
+}
+
+//
+if(20>30){
+    console.log("wrong");
+} else if(10<20){
+   console.log("right");
+}else {
+    console.log("choose another");
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
